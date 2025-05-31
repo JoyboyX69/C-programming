eee6eacf1dd31d5dc7eabe5f268ca0984a4eaf62
Ankush <psrankush@gmail.com>
2025-05-31 19:51:29 +0530

    Q27 is complete

---
+++ Practice Question/27_question.c
@@ -0,0 +1,19 @@
+//  this question is only for knowledge purpose of increament and Decreament
+
+// header file
+#include<stdio.h>
+
+// main file
+int main()
+{
+    // take a variable for input is 4
+    int a=4;
+
+    // check the increament
+    printf("%d %d %d",a,++a,a++);
+    // right to left=6 6 4
+    // left to right=4 5 5
+
+    return 0;
+    // end of code
+}
